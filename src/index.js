17b0efe091ee9ba207703a545659625f1c0bd39d
Assaf <assafyechiel@gmail.com>
2025-01-28 20:14:49 +0200

    Add comments

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownArrow = document.querySelector("#arrow");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
+// Toggle dropdown function
 const toggleDropdown = () => {
   dropdownMenu.classList.toggle("show");
   dropdownArrow.classList.toggle("bx-chevron-down");
   dropdownArrow.classList.toggle("bx-chevron-up");
 }
 
+// Toggle dropdown open/close when dropdown button is clicked
 dropdownButton.addEventListener('click', (e) => {
   console.log("button clicked")
   e.stopPropagation();
   toggleDropdown();
 });
 
+// Close dropdown when dom element is clicked
 document.documentElement.addEventListener('click', () => {
   if (dropdownMenu.classList.contains("show")) {
     toggleDropdown();
