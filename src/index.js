cd15844804bc26b9d1a805b3f6b91b9f7ca43aa5
Assaf <assafyechiel@gmail.com>
2025-01-28 19:01:56 +0200

    Create toggleDropdown function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import "./style.css";
-import { test } from "./home.js";
-
-console.log(test);
 
 if (process.env.NODE_ENV !== "production") {
-    console.log("Looks like we are in development mode!");
-  }
+  console.log("Looks like we are in development mode!");
+}
+
+const dropdownButton = document.querySelector(".dropdown-button");
+const dropdownArrow = document.querySelector("#arrow");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+const toggleDropdown = () => {
+  dropdownMenu.classList.toggle("show");
+  dropdownArrow.classList.toggle("bx-chevron-down");
+  dropdownArrow.classList.toggle("bx-chevron-up");
+}
+
+dropdownButton.addEventListener('click', (e) => {
+  console.log("button clicked")
+  e.stopPropagation();
+  toggleDropdown();
+});
